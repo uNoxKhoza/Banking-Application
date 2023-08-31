eff4705ede4ce3847cf58b226eca44d046eed94a
XoloKhoza <noxkhoza88@gmail.com>
2023-08-31 11:40:15 +0200

    Added deposit and withdraw

--- src/main/java/BankingApp.java
+++ src/main/java/BankingApp.java
@@ -1,3 +1,26 @@
+import java.util.Scanner;
+
 public class BankingApp {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner ( System.in );
+    }
+}
+class BankAccount{
+    int balance;
+    int previousTransaction;
+
+    void deposit(int amount){
+        if ( amount != 0 ){
+            balance = balance+ amount;
+            previousTransaction = amount;
+        }
+    }
+    void withdraw(int amount){
+        if(amount != 0){
+            balance = balance - amount;
+            previousTransaction = -amount;
+        }
+
+    }
 }
